85c900bed9089884698e7b248e237837e37cd8d5
Saeji Hong <68675994+tylereriksen@users.noreply.github.com>
2023-06-21 14:58:15 -0700

    Update AC.py

--- AC.py
+++ AC.py
@@ -1,92 +1,134 @@
-# Import necessary packages
-import gymnasium as gym
-import numpy as np
+# import necessary packages
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
+import torch.distributions as distributions
 import matplotlib.pyplot as plt
+import gym
 
 '''
-Observation: there seems to be a lot more variance in the total_rewards
+Seems to have a lot of variance in training
 '''
 
-# Define constants
+# define some constants
 ENV_NAME = 'CartPole-v1'
-LAYER1 = 64
-LAYER2 = 64
-EPISODES = 1000
-DISCOUNT = 0.999
+LAYER1 = 128
+LAYER2 = 128
+DISCOUNT = 0.99
 LEARNING_RATE = 0.001
+EPISODES = 400
 
-# Define the actor-critic network
-class ActorCritic(nn.Module):
-    def __init__(self, state_dim, action_dim):
-        super(ActorCritic, self).__init__()
-        self.fc1 = nn.Linear(state_dim, LAYER1)
+# Neural Net
+class NeuralNet(nn.Module):
+    def __init__(self, state_size: int, action_size: int):
+        super(NeuralNet, self).__init__()
+
+        # define the layers
+        self.fc1 = nn.Linear(state_size, LAYER1)
         self.fc2 = nn.Linear(LAYER1, LAYER2)
-        self.fc_pi = nn.Linear(LAYER2, action_dim)
-        self.fc_v = nn.Linear(LAYER2, 1)
+        self.fc3 = nn.Linear(LAYER2, action_size)
 
+    # step through function
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.dropout(x, p=0.4)
+        x = self.fc3(x)
+        return x
+
+# define the Actor-Critic Model
+class ActorandCritic(nn.Module):
+    def __init__(self, actor: NeuralNet, critic: NeuralNet):
+        super().__init__()
+        self.actor = actor
+        self.critic = critic
+
+    #  output prediction function
     def forward(self, state):
-        x = torch.relu(self.fc1(state))
-        x = torch.relu(self.fc2(x))
-        pi = torch.softmax(self.fc_pi(x), dim=0)
-        v = self.fc_v(x)
+        policy_output = self.actor(state)
+        value_output = self.critic(state)
+        return policy_output, value_output
 
-        return pi, v
+# get all the rewards collected and reverse propagate with the Values
+def compute_returns(rewards: list, discount=DISCOUNT):
+    computed_returns = []
+    accumulated = 0
+    for idx, val in enumerate(reversed(rewards)):
+        computed_returns.insert(0, val + discount * accumulated)
+        accumulated = computed_returns[0]
+    computed_returns = torch.tensor(computed_returns)
+    return computed_returns
+
+# update the actor-critic model
+def update(returns, log_prob_actions, values, optimizer):
+    returns = returns.detach()
+    policy_loss = - (returns * log_prob_actions).sum()
+    value_loss = F.smooth_l1_loss(returns, values).sum()
+    optimizer.zero_grad()
+
+    total_loss = policy_loss + value_loss
+    total_loss.backward()
+
+    optimizer.step()
+    return policy_loss.item(), value_loss.item()
 
 
-# Define the environment and other parameters
+# Create the environment
 env = gym.make(ENV_NAME)
 
-# Initialize the ActorCritic network
-agent = ActorCritic(env.observation_space.shape[0], env.action_space.n)
+# Observation Space: cart position, cart velocity, pole angle, pole angular velocity
+state_size = env.observation_space.shape[0]
+action_size = env.action_space.n
 
-# Define the optimizer
-optimizer = optim.Adam(agent.parameters(), lr=LEARNING_RATE)
+print(env.observation_space.high) # [4.8000002e+00 3.4028235e+38 4.1887903e-01 3.4028235e+38]
+print(env.observation_space.low) # [-4.8000002e+00 -3.4028235e+38 -4.1887903e-01 -3.4028235e+38]
 
-total_rewards = []
+# define the variables
+actor, critic = NeuralNet(state_size, action_size), NeuralNet(state_size, 1)
+agentPol = ActorandCritic(actor, critic)
+agentPol.load_state_dict(agentPol.state_dict())
+optimizer = optim.Adam(agentPol.parameters(), lr=LEARNING_RATE)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Define the training loop
-for episode in range(EPISODES):
-    # Initialize the environment
-    state, _ = env.reset()
+# iterate through the different episodes
+train_rewards = []
+for i in range(EPISODES):
+    agentPol.train()
+    log_prob_actions, values, rewards = [], [], []
     done = False
     total_reward = 0
+    state = env.reset()
 
     while not done:
-        # Select an action using the agent's policy
-        probs, val = agent(torch.tensor(state, dtype=torch.float32))
-        action = np.random.choice(np.arange(len(probs)), p=probs.detach().numpy())
+        state = torch.FloatTensor(state).unsqueeze(0).to(device)
+        policy_output, value_output = agentPol(state) # get the predictions
+        action_prob = F.softmax(policy_output, dim=-1) # get the action probabilities
+        distribution = distributions.Categorical(action_prob)
+        action = distribution.sample() # get a sample from the action probabilities distribution
 
-        # Take a step in the environment
-        next_state, reward, done, _, _ = env.step(action)
-        total_reward += reward
+        log_prob_action = distribution.log_prob(action)
+        state, reward, done, _ = env.step(action.item()) # take the action as a step in environment
 
-        # Calculate the TD error and loss
-        _, next_val = agent(torch.tensor(next_state, dtype=torch.float32))
-        expected_return = reward + DISCOUNT * next_val * (1 - done)
-        td_error = expected_return - val
-        actor_loss = -torch.log(probs[action]) * td_error
-        critic_loss = torch.square(td_error)
-        loss = actor_loss + critic_loss
+        # add these values to their respective lists
+        log_prob_actions.append(log_prob_action)
+        rewards.append(reward)
+        values.append(value_output)
 
-        # Update the network
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        total_reward += reward # add rewards
 
-        # Set the state to the next state
-        state = next_state
-        if total_reward >= 500: # capping it at 500 since this environment can go as high as it can
-            break
+    log_prob_actions = torch.cat(log_prob_actions)
+    values = torch.cat(values).squeeze(-1)
+    computed_returns = compute_returns(rewards, DISCOUNT)
+    loss = update(computed_returns, log_prob_actions, values, optimizer)
 
-    # Print the total reward for the episode
-    print(f'Episode {episode}: Total reward = {total_reward}')
-    total_rewards.append(total_reward)
+    train_rewards.append(total_reward)
+    print('episode: ', i)
+    print('train reward: ', total_reward)
 
-plt.plot(range(len(total_rewards)), total_rewards)
-plt.xlabel('Episode')
-plt.ylabel('Total Reward')
-plt.title('Total Rewards over Episodes')
+plt.plot(range(len(train_rewards)), train_rewards)
+plt.xlabel("Episodes")
+plt.ylabel("Rewards")
+plt.title("Rewards Collected Over Episodes")
 plt.show()
+

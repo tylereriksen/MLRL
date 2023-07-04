737f63cfee3897771458b5dced6feaf27d226d32
Saeji Hong <68675994+tylereriksen@users.noreply.github.com>
2023-07-04 13:52:41 -0700

    Update DQN.py

--- DQN.py
+++ DQN.py
@@ -1,8 +1,22 @@
 import numpy as np
 import gym
+import random
+
+EPSILON = 1.0
+EPSILON_DECAY = 0.99
+EPSILON_MIN = 0.05
+MIN_REPLAY_MEMORY_SIZE = 1_000
+REPLAY_MEMORY_SIZE = 10_000
+BATCH_SIZE = 128
+LEARNING_RATE = 0.001
+TARGET_UPDATE_FREQUENCY = 5
+EPISODES = 100
+HIDDEN_LAYER1 = 64
+HIDDEN_LAYER2 = 64
+
 
 class NeuralNetwork:
-    def __init__(self, input_size, output_size, hidden1, hidden2, lr=0.001):
+    def __init__(self, input_size, output_size, hidden1, hidden2, lr=LEARNING_RATE):
         self.input_size = input_size
         self.output_size = output_size
         self.hidden1 = hidden1
@@ -18,8 +32,8 @@ class NeuralNetwork:
 
     def forward(self, x):
 
-        def relu(x):
-            return np.maximum(0, x)
+        def relu(i):
+            return np.maximum(0, i)
 
         self.z1 = np.dot(x, self.weights1) + self.bias1
         self.a1 = relu(self.z1)
@@ -44,7 +58,7 @@ class NeuralNetwork:
         self.forward(x)
         dL_dz3 = [[0, -2 * (y_j - self.z3[0][1])]] if action_taken == 1 else [[-2 * (y_j - self.z3[0][0]), 0]]
         dL_dw3 = np.dot(self.a2.T, dL_dz3) # multiply dz3_dw3 with dL_dz3
-        dL_db3 = dL_dz3
+        dL_db3 = np.array(dL_dz3)
 
         dL_dz2 = np.dot(dL_dz3, self.weights3.T) * relu_derivative(self.z2)
         dL_dw2 = np.dot(self.a1.T, dL_dz2)
@@ -54,13 +68,16 @@ class NeuralNetwork:
         dL_dw1 = np.dot(x.T, dL_dz1)
         dL_db1 = dL_dz1
 
-        self.weights3 -= self.lr * dL_dw3
-        self.biases3 -= self.lr * dL_db3
-        self.weights2 -= self.l1 * dL_dw2
-        self.biases2 -= self.lr * dL_db2
-        self.weights1 -= self.lr * dL_dw1
-        self.biases1 -= self.lr * dL_db1
+               # 64-2,
+        return [dL_dw3, dL_db3, dL_dw2, dL_db2, dL_dw1, dL_db1]
 
+    def update_parameters(self, dw3, db3, dw2, db2, dw1, db1):
+        self.weights3 -= self.lr * dw3
+        self.bias3 -= self.lr * db3
+        self.weights2 -= self.lr * dw2
+        self.bias2 -= self.lr * db2
+        self.weights1 -= self.lr * dw1
+        self.bias1 -= self.lr * db1
 
 
 class ReplayMemory:
@@ -79,35 +96,76 @@ class ReplayMemory:
     def getMemory(self):
         return self.memory
 
+    def returnBatch(self, batch_size):
+        if len(self.memory) < MIN_REPLAY_MEMORY_SIZE:
+            return None
+        minibatch = random.sample(self.memory, batch_size)
+        return minibatch
 
 
-actionValFunc = NeuralNetwork(4, 2, 64, 64)
-targetValFunc = NeuralNetwork(4, 2, 64, 64)
-targetValFunc.set_equal_parameters(actionValFunc)
-memory = ReplayMemory(10000)
-EPSILON = 1.0
-EPSILON_DECAY = 0.99
-step_count = 0
-
+# Create the environment
 env = gym.make("CartPole-v1")
-for episode in range(10):
+
+# Observation Space: cart position, cart velocity, pole angle, pole angular velocity
+state_size = env.observation_space.shape[0]
+action_size = env.action_space.n
+
+print(env.observation_space.high) # [4.8000002e+00 3.4028235e+38 4.1887903e-01 3.4028235e+38]
+print(env.observation_space.low) # [-4.8000002e+00 -3.4028235e+38 -4.1887903e-01 -3.4028235e+38]
+
+actionValFunc = NeuralNetwork(state_size, action_size, HIDDEN_LAYER1, HIDDEN_LAYER2)
+targetValFunc = NeuralNetwork(state_size, action_size, HIDDEN_LAYER1, HIDDEN_LAYER2)
+targetValFunc.set_equal_parameters(actionValFunc)
+memory = ReplayMemory(REPLAY_MEMORY_SIZE)
+
+step_count = 0
+for episode in range(EPISODES):
     state = env.reset()
     done = False
     total_reward = 0
     while not done:
+        # get action
         if np.random.rand() <= EPSILON:
-            action = np.random.choice(2)
+            action = np.random.choice(action_size)
         else:
             action = np.argmax(actionValFunc.forward(state))
-        EPSILON = np.maximum(EPSILON * EPSILON_DECAY, 0.05)
+        # update epsilon
+        EPSILON = np.maximum(EPSILON * EPSILON_DECAY, EPSILON_MIN)
+
         next_state, reward, done, _ = env.step(action)
         total_reward += reward
         memory.append((state, action, reward, next_state, done))
 
-        y_j = reward if done else reward + np.max(targetValFunc.forward(next_state))
-        actionValFunc.backward(state, y_j, action)
+        minibatch = memory.returnBatch(BATCH_SIZE)
+
+        if minibatch is None:
+            step_count += 1
+            state = next_state
+            continue
+
+        gradients = {"w3": [], "b3": [], "w2": [], "b2": [], "w1": [], "b1" : []}
+        for transition in minibatch:
+            y_j = transition[2] if transition[4] else transition[2] + np.max(targetValFunc.forward(transition[3]))
+            gradient = actionValFunc.backward(np.array([transition[0]]), y_j, transition[1])
+            gradients["w3"].append(gradient[0].tolist())
+            gradients["b3"].append(gradient[1].tolist())
+            gradients["w2"].append(gradient[2].tolist())
+            gradients["b2"].append(gradient[3].tolist())
+            gradients["w1"].append(gradient[4].tolist())
+            gradients["b1"].append(gradient[5].tolist())
+
+        dw3, db3 = np.mean(np.array(gradients["w3"]), axis=0), np.mean(np.array(gradients["b3"]), axis=0)
+        dw2, db2 = np.mean(np.array(gradients["w2"]), axis=0), np.mean(np.array(gradients["b2"]), axis=0)
+        dw1, db1 = np.mean(np.array(gradients["w1"]), axis=0), np.mean(np.array(gradients["b1"]), axis=0)
+
+        actionValFunc.update_parameters(dw3, db3, dw2, db2, dw1, db1)
 
         state = next_state
 
-        if step_count % 5 == 0:
+        if step_count % TARGET_UPDATE_FREQUENCY == 0:
             targetValFunc.set_equal_parameters(actionValFunc)
+
+        step_count += 1
+
+
+env.close()

01d53ca254e24fe4061c5dc099df6bbac994679c
Saeji Hong <68675994+tylereriksen@users.noreply.github.com>
2023-07-06 22:27:17 -0700

    Update DQN.py

--- DQN.py
+++ DQN.py
@@ -1,38 +1,43 @@
+# import necessary packages
 import numpy as np
 import gym
 import random
 import matplotlib.pyplot as plt
 
+# define necessary constants and variables
 EPSILON = 1.0
-EPSILON_DECAY = 0.99
+EPSILON_DECAY = 0.995
 EPSILON_MIN = 0.05
-MIN_REPLAY_MEMORY_SIZE = 1_000
+MIN_REPLAY_MEMORY_SIZE = 3_000
 REPLAY_MEMORY_SIZE = 10_000
-BATCH_SIZE = 128
+BATCH_SIZE = 256
 LEARNING_RATE = 0.001
 TARGET_UPDATE_FREQUENCY = 5
-EPISODES = 200
+EPISODES = 300
 HIDDEN_LAYER1 = 64
 HIDDEN_LAYER2 = 64
 
 
+# define the Neural Net Class
 class NeuralNetwork:
     def __init__(self, input_size, output_size, hidden1, hidden2, lr=LEARNING_RATE):
-        self.input_size = input_size
-        self.output_size = output_size
-        self.hidden1 = hidden1
-        self.hidden2 = hidden2
-        self.lr = lr
+        self.input_size = input_size # input size
+        self.output_size = output_size # output size
+        self.hidden1 = hidden1 # hidden layer 1 size
+        self.hidden2 = hidden2 # hidden layer 2 size
+        self.lr = lr # learning rate
 
-        self.weights1 = np.random.randn(self.input_size, self.hidden1) # (4, 64)
+        self.weights1 = np.random.randn(self.input_size, self.hidden1) * np.sqrt(2 / (self.input_size + self.output_size)) # (4, 64)
         self.bias1 = np.zeros((1, self.hidden1)) # (1, 64)
-        self.weights2 = np.random.randn(self.hidden1, self.hidden2) # (64, 64)
+        self.weights2 = np.random.randn(self.hidden1, self.hidden2) * np.sqrt(2 / (self.input_size + self.output_size))# (64, 64)
         self.bias2 = np.zeros((1, self.hidden2)) # (1, 64)
-        self.weights3 = np.random.randn(self.hidden2, self.output_size) # (64, 2)
+        self.weights3 = np.random.randn(self.hidden2, self.output_size) * np.sqrt(2 / (self.input_size + self.output_size))# (64, 2)
         self.bias3 = np.zeros((1, self.output_size)) # (1, 2)
 
-    def forward(self, x):
+        self.z1, self.a1, self.z2, self.a2, self.z3 = None, None, None, None, None # set other outputs of layers to None
 
+    def forward(self, x):
+        # define rectified linear function
         def relu(i):
             return np.maximum(0, i)
 
@@ -45,29 +50,30 @@ class NeuralNetwork:
         return self.z3
 
     def set_equal_parameters(self, otherNet):
-        self.weights1 = np.copy(otherNet.weights1)
-        self.bias1 = np.copy(otherNet.bias1)
-        self.weights2 = np.copy(otherNet.weights2)
-        self.bias2 = np.copy(otherNet.bias2)
-        self.weights3 = np.copy(otherNet.weights3)
-        self.bias3 = np.copy(otherNet.bias3)
+        self.weights1 = np.array([i for i in list(otherNet.weights1)])
+        self.bias1 = np.array([i for i in list(otherNet.bias1)])
+        self.weights2 = np.array([i for i in list(otherNet.weights2)])
+        self.bias2 = np.array([i for i in list(otherNet.bias2)])
+        self.weights3 = np.array([i for i in list(otherNet.weights3)])
+        self.bias3 = np.array([i for i in list(otherNet.bias3)])
+
 
     def backward(self, x, y_j, action_taken):
         def relu_derivative(i):
             return np.where(i > 0, 1, 0)
 
         self.forward(x)
-        dL_dz3 = [[0, -2 * (y_j - self.z3[0][1])]] if action_taken == 1 else [[-2 * (y_j - self.z3[0][0]), 0]]
-        dL_dw3 = np.dot(self.a2.T, dL_dz3) # multiply dz3_dw3 with dL_dz3
-        dL_db3 = np.array(dL_dz3)
+        dL_dz3 = np.array([[0, -2 * (y_j - self.z3[0][1])]]) if action_taken == 1 else np.array([[-2 * (y_j - self.z3[0][0]), 0]]) # (1, 2)
+        dL_dw3 = np.dot(self.a2.T, dL_dz3) # (64, 2)
+        dL_db3 = np.array(dL_dz3) # (1, 2)
 
-        dL_dz2 = np.dot(dL_dz3, self.weights3.T) * relu_derivative(self.z2)
-        dL_dw2 = np.dot(self.a1.T, dL_dz2)
-        dL_db2 = dL_dz2
+        dL_dz2 = np.dot(dL_dz3, self.weights3.T) * relu_derivative(self.z2) # (1, 64)
+        dL_dw2 = np.dot(dL_dz2.T, self.a1).T # (64, 64)
+        dL_db2 = dL_dz2 # (1, 64)
 
-        dL_dz1 = np.dot(dL_dz2, self.weights2.T) * relu_derivative(self.z1)
-        dL_dw1 = np.dot(x.T, dL_dz1)
-        dL_db1 = dL_dz1
+        dL_dz1 = np.dot(dL_dz2, self.weights2.T) * relu_derivative(self.z1) # (1, 64)
+        dL_dw1 = np.dot(dL_dz1.T, x).T # (4, 64)
+        dL_db1 = dL_dz1 # (1, 64)
 
         return [dL_dw3, dL_db3, dL_dw2, dL_db2, dL_dw1, dL_db1]
 
@@ -131,7 +137,8 @@ for episode in range(EPISODES):
         state = next_state
     baseline_rewards.append(total_reward)
 
-plt.plot(range(1, EPISODES + 1), baseline_rewards, 'r-')
+plt.plot(range(1, EPISODES + 1), baseline_rewards, 'k-')
+print(f"Average Pre-Training Reward: {np.mean(baseline_rewards)}")
 
 
 step_count = 0
@@ -190,6 +197,24 @@ for episode in range(EPISODES):
 
 plt.plot(range(1, EPISODES + 1), testing_rewards, 'b-')
 plt.show()
+
+
+print(f"Average Pre-Training Reward: {np.mean(baseline_rewards)}")
+
+baseline_rewards = []
+for episode in range(EPISODES):
+    state = env.reset()
+    done = False
+    total_reward = 0
+    while not done:
+        action = np.argmax(actionValFunc.forward(state))
+        next_state, reward, done, _ = env.step(action)
+        total_reward += reward
+        state = next_state
+    baseline_rewards.append(total_reward)
+
+plt.plot(range(1, EPISODES + 1), baseline_rewards, 'k-')
+print(f"Average Post-Training Reward: {np.mean(baseline_rewards)}")
 
 
 env.close()

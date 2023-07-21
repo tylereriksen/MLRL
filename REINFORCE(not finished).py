6ea128a7c4cf256fce222c8bf8ced2cb7bafc4a1
Saeji Hong <68675994+tylereriksen@users.noreply.github.com>
2023-07-21 15:48:59 -0700

    Update REINFORCE(not finished).py

--- REINFORCE(not finished).py
+++ REINFORCE(not finished).py
@@ -1,140 +1,121 @@
-# import necessary packages
-import gym
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
+import gym
 
-env = gym.make("CartPole-v1")
+env = gym.make('CartPole-v1')
 
-def relu(mat):
-    return np.multiply(mat, (mat > 0))
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(NeuralNet, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, output_size)
 
-def relu_derivative(mat):
-    return (mat > 0) * 1
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
+    def apply_softmax(self, x):
+        return F.softmax(x, dim=1)
 
-class NNLayer:
-    def __init__(self, input_size, output_size, activation=None, lr=0.001):
-        self.input_size = input_size
-        self.output_size = output_size
+    def choose_action(self, probs):
+        action = torch.multinomial(probs, 1).item()
+        return action
 
-        self.weights = np.random.uniform(low=-0.5, high=0.5, size=(input_size, output_size))  # initialize weights
-        self.activation_function = activation
+    def get_log_prob_gradient(self, state, action):
+        # Pass state through the neural network
+        output = self.forward(state)
 
-        self.lr = lr
+        # Apply softmax to obtain action probabilities
+        softmax_probs = self.apply_softmax(output)
 
-    def forward(self, inputs, remember_for_backprop=True):
-        input_with_bias = np.append(inputs, 1)
-        unactivated = None
-        unactivated = np.dot(input_with_bias, self.weights)
-        output = unactivated
-        if self.activation_function != None:
-            output = self.activation_function(output)
-        if remember_for_backprop:
-            self.backward_store_in = input_with_bias
-            self.backward_store_out = np.copy(unactivated)
-        return output
+        # Calculate log-probabilities of actions
+        log_probs = torch.log(softmax_probs)
 
-    def update_weights(self, gradient, gamma, cumulated_reward, t):
-        self.weights += self.lr * (gamma ** t) * cumulated_reward * gradient
+        # Select the log-probability of the chosen action (action is a scalar here)
+        log_prob_selected_action = log_probs[0, action]
 
+        # Compute gradients of the selected log-probability with respect to the neural network parameters
+        log_prob_selected_action.backward()
 
-    def backward(self, gradient_from_above, gamma, cumulated_return, t):
-        adjusted_mul = gradient_from_above
-        if self.activation_function == None:
-            adjusted_mul = np.multiply(relu_derivative(self.backward_store_out), gradient_from_above)
-        D_i = np.dot(np.transpose(np.reshape(self.backward_store_in, (1, len(self.backward_store_in)))),
-                     np.reshape(adjusted_mul, (1, len(adjusted_mul))))
-        delta_i = np.dot(adjusted_mul, np.transpose(self.weights))[:-1]
-        self.update_weights(D_i, gamma, cumulated_return, t)
-        return delta_i
+        # Access gradients
+        gradients = []
+        for param in self.parameters():
+            gradients.append(param.grad.clone())
 
+        # Clear gradients for next computation
+        self.zero_grad()
 
-class RLAgent:
-    env = None
-    def __init__(self, env, num_hidden_layers=2, hidden_size=24, gamma=0.95):
-        self.env = env
-        self.hidden_size = hidden_size
-        self.input_size = env.observation_space.shape[0]
-        self.output_size = env.action_space.n
-        self.num_hidden_layers = num_hidden_layers
-        self.gamma = gamma
-        self.layers = [
-            NNLayer(self.input_size + 1, self.hidden_size, activation=relu),
-            NNLayer(self.hidden_size + 1, self.hidden_size, activation=relu),
-            NNLayer(self.hidden_size + 1, self.output_size)
-        ]
-
-    def select_action(self, observation):
-        values = self.forward(np.asmatrix(observation), False)
-        action_prob = np.exp(values - np.max(values)) / np.sum(np.exp(values - np.max(values)))
-        if np.random.random() < action_prob[0]:
-            return 0
-        return 1
-
-    def get_returns(self, rewards_list):
-        returns = []
-        G = 0
-        for r in reversed(rewards_list):
-            G = r + self.gamma * G
-            returns.insert(0, G)
-        return returns
-
-    def forward(self, observation, remember_for_backprop=True):
-        vals = np.copy(observation)
-        index = 0
-        for layer in self.layers:
-            vals = layer.forward(vals, remember_for_backprop)
-            index = index + 1
-        return vals
-
-    def backward(self, observation, action, cumulated_return, t):
-        values = self.forward(np.asmatrix(observation), True)
-        delta = np.zeros_like(values)
-        delta[action] = 1 / values[action]
-
-        for layer in reversed(self.layers):
-            delta = layer.backward(delta, self.gamma, cumulated_return, t)
+        return gradients
 
 
 
-
-NUM_EPISODES = 10_000
-MAX_TIMESTEPS = 1_000
-GAMMA = 0.95
-hidden_layer_size = 64
-num_hidden_layers = 2
-
-model = RLAgent(env, num_hidden_layers, hidden_layer_size, GAMMA)
-
-for i_episode in range(NUM_EPISODES):
-    rewards_list = []
-    actions_list = []
-    states_list = []
-    observation = env.reset()
-    states_list.append(observation)
-    for t in range(MAX_TIMESTEPS):
-        action = model.select_action(observation)
-        prev_obs = observation
-        observation, reward, done, info = env.step(action)
-        rewards_list.append(reward)
-        actions_list.append(action)
-        states_list.append(observation)
-        if done:
-            print('Episode {} ended with total reward of {}'.format(i_episode, t))
-            break
-
-    G_t = model.get_returns(rewards_list)
-    for i in range(len(G_t)):
-        prev_obs, observation, action, reward = states_list[i], states_list[i + 1], actions_list[i], G_t[i]
-        model.backward(prev_obs, action, reward, i + 1)
+def calculate_cumulative_rewards(rewards):
+    returns = []
+    tot = 0
+    for r in reversed(rewards):
+        tot = r + 0.99 * tot
+        returns.insert(0, tot)
+    return returns
 
 
-observation = env.reset()
-for t in range(MAX_TIMESTEPS):
-    action = model.select_action(observation)
-    prev_obs = observation
-    observation, reward, done, info = env.step(action)
-    if done:
-        break
+DISCOUNT = 0.99
+LEARNING_RATE = 0.01
 
-print("Done")
+
+# Usage example
+input_size = env.observation_space.shape[0]
+output_size = env.action_space.n
+hidden_size = 64
+
+policy_net = NeuralNet(input_size, hidden_size, output_size)
+
+
+for episode in range(25):
+    done = False
+    state = env.reset()
+    state = torch.from_numpy(np.array(state)).float()
+    state = state.unsqueeze(0)
+    episode_rewards = []
+    episode_actions = []
+    episode_states = [state]
+    total_rewards = 0
+    while not done:
+        action = policy_net.choose_action(policy_net.apply_softmax(policy_net.forward(state)))
+        new_state, reward, done, _ = env.step(action)
+        total_rewards += reward
+        new_state = torch.from_numpy(np.array(new_state)).float()
+        new_state = new_state.unsqueeze(0)
+        episode_rewards.append(reward)
+        episode_actions.append(action)
+        episode_states.append(new_state)
+        new_state = state
+
+    discounted_values = calculate_cumulative_rewards(episode_rewards)
+    for i in range(len(episode_actions)):
+        curr_update_state = episode_states[i]
+        curr_update_reward = discounted_values[i]
+        curr_update_action = episode_actions[i]
+
+        gradients = policy_net.get_log_prob_gradient(curr_update_state, curr_update_action)
+
+        with torch.no_grad():
+            for param, gradient in zip(policy_net.parameters(), gradients):
+                param += LEARNING_RATE * (DISCOUNT ** (i + 1)) * curr_update_reward * gradient
+    print(f'Total Reward for Episode {episode} was {total_rewards}')
+    print(f'States were {str(episode_states)}')
+    print(f'Rewards were {str(episode_rewards)}')
+    print(f'Actions were {str(episode_actions)}')
+    print()
+
+
+env.close()
+
+
+
+
 
